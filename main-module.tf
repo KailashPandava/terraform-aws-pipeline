6defb70f58878762f781042ed66dc367b5107f13
Kailash Pandava <31236544+KailashPandava@users.noreply.github.com>
2022-11-11 15:32:31 +0530

    Update main-module.tf

--- main-module.tf
+++ main-module.tf
@@ -18,10 +18,3 @@ output "test" {
   sensitive = true
 }
 
-  resource "aws_instance" "KP" {
-  ami           = "ami-09d3b3274b6c5d4aa"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "LBG test"
-  }
-}
